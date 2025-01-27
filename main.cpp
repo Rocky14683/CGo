c2af9f0f8f492edfad17643faae91fa9cebd8a7d
Rocky14683 <rocky10208@gmail.com>
2025-01-26 22:45:54 -0500

    even more funny work

--- main.cpp
+++ main.cpp
@@ -1,32 +1,65 @@
 #include <iostream>
 #include <queue>
+#include <utility>
+#include <type_traits>
 
-template <class T> struct larrow {
-        larrow(T* a_) : a(a_) {}
-        T* a;
+#include <string>
+#include <type_traits>
+
+
+
+namespace type_traits {
+//SFINAE stuffs
+template <class T> struct is_serializable : std::false_type {};
+
+template <class T>
+requires requires(T x) {
+    { std::declval<std::ostream&>() << x };
+} struct is_serializable<T> : std::true_type {};
+
+template <class T> struct is_serializable_nonptr
+    : std::bool_constant<!std::is_pointer_v<T> && is_serializable<T>::value> {};
 };
 
-template <class T> larrow<T> operator-(T& a) { return larrow<T>(&a); }
 
 
-template <class T> class Channel {
+template <class T> struct larrow {
+        T data;
+        larrow(T x) : data(std::move(x)) {}
+};
+
+template <class U>
+    requires type_traits::is_serializable_nonptr<U>::value
+larrow<U> operator-(U&& x) {
+    return larrow<U>(std::forward<U>(x));
+}
+
+template <class T>
+    requires type_traits::is_serializable_nonptr<T>::value
+class Channel {
     public:
         std::queue<T> data;
 
-        Channel() : data() {}
+        static Channel<T> make_chan() { return Channel<T>(); }
 
-        static Channel<std::string> make_chan() { return Channel<T> {}; }
-
-        template <class U> friend void operator<(Channel<U>& lhs, const larrow<U>& it);
+        friend void operator<(Channel<T>& lhs, const larrow<T>& rhs) { lhs.data.push(rhs.data); }
 };
 
-template <class U> void operator<(Channel<U>& lhs, const larrow<U>& it) {
-    std::print("pushing {}\n", *it.a);
-    return lhs.data.push(*it.a);
+template <class T> void drain(Channel<T>& chan) {
+    while (!chan.data.empty()) {
+        std::println("{}", chan.data.front());
+        chan.data.pop();
+    }
 }
 
 int main() {
-    auto chan = Channel<std::string>::make_chan();
-    std::string s = "hello";
-    chan <- s;
+    auto chanInt = Channel<int>::make_chan();
+    chanInt <- 42;
+    drain(chanInt);
+
+    auto chanString = Channel<std::string>::make_chan();
+    chanString <- std::string("Hello, World!");
+    drain(chanString);
+
+    return 0;
 }
